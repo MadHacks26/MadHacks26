624083f39edde047f507cb93cdaa3414b1d9f3c7
Chu1503 <shivakash2003@gmail.com>
2026-02-22 08:20:33 -0600

    cheese

--- madhacks-frontend/src/App.tsx
+++ madhacks-frontend/src/App.tsx
@@ -7,68 +7,79 @@ import MockInterview from "./pages/MockInterview";
 import MockPrepScreen from "./pages/MockPrepScreen.tsx";
 import Auth from "./pages/Auth";
 import FeedbackScreen from "./pages/FeedbackScreen.tsx";
-
 import Home from "./pages/Home.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>} 
-          />
-          <Route path="/auth" element={<Auth />} />
-          <Route
-            path="/summary"
-            element={
-              <ProtectedRoute>
-                <Summary />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/create"
-            element={
-              <ProtectedRoute>
-                <Create />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/roadmap"
-            element={
-              <ProtectedRoute>
-                <Roadmap />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/mock-interview"
-            element={
-              <ProtectedRoute>
-                <MockPrepScreen />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/mock-interview/session"
-            element={
-              <ProtectedRoute>
-                <MockInterview />
-              </ProtectedRoute>
-            }
-          />
-          <Route 
-          path="/mock-feedback"   
-          element={
-          <ProtectedRoute>
-            <FeedbackScreen />
-            </ProtectedRoute>} />
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-1">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/auth" element={<Auth />} />
+              <Route
+                path="/summary"
+                element={
+                  <ProtectedRoute>
+                    <Summary />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/create"
+                element={
+                  <ProtectedRoute>
+                    <Create />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/roadmap"
+                element={
+                  <ProtectedRoute>
+                    <Roadmap />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mock-interview"
+                element={
+                  <ProtectedRoute>
+                    <MockPrepScreen />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mock-interview/session"
+                element={
+                  <ProtectedRoute>
+                    <MockInterview />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mock-feedback"
+                element={
+                  <ProtectedRoute>
+                    <FeedbackScreen />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </div>
 
-        </Routes>
+          <footer className="text-center text-sm text-white py-6">
+            Made with 🧀 in Madison
+          </footer>
+        </div>
       </AuthProvider>
     </BrowserRouter>
   );
